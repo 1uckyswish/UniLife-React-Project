49155feb2bfd19e92e5aa76c8d48ecf769fc1518
Noel Guillen <noelblas274@gmail.com>
2023-06-10 16:05:23 -0400

    footer styles & header styles complete

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
 import './Footer.css'
+import { BsFacebook } from "react-icons/bs";
+import { AiFillTwitterCircle, AiFillInstagram} from "react-icons/ai";
 
 function Footer() {
   return (
-    <div>
-      <h1>footer</h1>
+    <div className="footer-container">
+      <div className='footer-container-items'>
+         <div className='newsletter-container'>
+            <h2>Keep in touch</h2>
+            <p>Curious about new offerings? Sign up for our <br/>weekly newsletter and stay informed.</p>
+            <input type="email" placeholder='Your Email'/>
+      </div>
+        <div className='social-container'>
+            <h2>Let’s Socialize </h2>
+              <div className='social-icons-container'>
+                <p><BsFacebook className='social-icon'/> Facebook</p>
+                <p><AiFillTwitterCircle className='social-icon'/> Twitter</p>
+                <p><AiFillInstagram className='social-icon'/> Instagram</p>
+              </div>
+        </div>
+      </div>
+        <div className="credits-container">
+          <div className="footer-links-container">
+            <p>About Us</p>
+            <p>Terms & Conditions</p>
+            <p>Privacy & Cookie Policies</p>
+          </div>
+          <div className='footer-copyright-container'>
+            <p>2022</p>
+            <p>&copy;Unilife</p>
+        </div>
+      </div>
     </div>
   )
 }
