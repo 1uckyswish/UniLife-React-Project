279b550471e53bcd8b582c515a3162a31f5803cc
Noel Guillen <noelblas274@gmail.com>
2023-06-11 15:50:32 -0400

    Completed the css for header banner

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,22 @@ import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
 import Homepage from './Pages/Homepage/Homepage';
 import Slider from './Components/Slider/Slider';
+import Data from "./utils/data.js";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
+  const [data1, data2, data3] = Data;
+
   return (
-    <div>
-      <Header />
-      <Slider />
-      <Homepage />
-      <Footer />
-    </div>
-  )
+ <BrowserRouter>
+    <Header />
+    <Slider data={data1} />
+    <Routes>
+      <Route path='/' element={<Homepage />} />
+    </Routes>
+    <Footer />
+ </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import './Slider.css';
 import Banner from '/src/assets/skyline-image.png';
-import data from '/src/utils/data.js';
 
-function Slider() {
-    console.log(data)
+function Slider({data}) {
+  const { title, description } = data;
+
   return (
     <div className='slider-container'>
       <img className='slider-image' src={Banner} alt='Skyline' />
       <div className='slider-header'>
-        <h2>Find student homes <br/>with bills included</h2>
-        <p>A simple and faster way to search for student accommodation</p>
+        <h2>{title}</h2>
+        <p>{description}</p>
       </div>
     </div>
   );
