883e0ac9971cbcff724d7482e0d383539aa4564d
Noel Guillen <noelblas274@gmail.com>
2023-06-11 22:13:07 -0400

    completed form for cities

---
+++ src/Components/TopCities/TopCities.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TopCities.css';
+
+function TopCities() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TopCities
--- src/Pages/Homepage/Homepage.jsx
+++ src/Pages/Homepage/Homepage.jsx
@@ -1,12 +1,53 @@
-import React from 'react'
-import './Homepage.css'
+import { useEffect, useState } from 'react';
+import './Homepage.css';
+import axios from 'axios';
 
 function Homepage() {
+  const citiesApiUrl1 = 'https://unilife-server.herokuapp.com/cities?page=1';
+  const citiesApiUrl2 = 'https://unilife-server.herokuapp.com/cities?page=2';
+  const [cities, setCities] = useState([]);
+
+  useEffect(() => {
+    axios.get(citiesApiUrl1)
+      .then((result) => {
+        console.log(result);
+        setCities((prevCities) => [...prevCities, ...result.data.response]);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  useEffect(() => {
+    axios.get(citiesApiUrl2)
+      .then((result) => {
+        console.log(result);
+        setCities((prevCities) => [...prevCities, ...result.data.response]);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <div className='homepage-container'>
-      <h1>Homepage</h1>
+    <div className='form-container'>
+      <select name="Search by city" id="cities">
+        <option value="Search by city">Search by city</option>
+        {
+          cities.map((item) => (
+            <option key={item.id} value={item.name}>
+              {item.name}
+            </option>
+          ))
+        }
+      </select>
+      <button type="submit" form="cities">Find Homes</button>
     </div>
-  )
+    <div className='top-cities-container'>
+      <h1>Student Accommodations In our Top Cities</h1>
+
+
+
+    </div>
+    </div>
+  );
 }
 
-export default Homepage
+export default Homepage;
