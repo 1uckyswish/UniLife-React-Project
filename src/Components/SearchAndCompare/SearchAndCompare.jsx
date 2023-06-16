a40664cf2f25f89aac00249d83f7c9e760a4d5c6
Noel Guillen <noelblas274@gmail.com>
2023-06-16 02:29:50 -0400

    details container with icons completed

--- src/Components/SearchAndCompare/SearchAndCompare.jsx
+++ src/Components/SearchAndCompare/SearchAndCompare.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
 import "./SearchAndCompare.css";
+import searchIco from "/src/assets/search-icon.png"
+import todoList from "/src/assets/task-list.png"
+import billList from "/src/assets/bill-icon.png"
+
+
 
 function SearchAndCompare() {
 
   return (
-    <div>
-      <p>Hello</p>
+    <div className='detail-icons-container'>
+      <h2>Compare all inclusive student homes.</h2>
+      <div className='detail-info-container'>
+        <div className="detail-icon-card">
+            <img src={searchIco} />
+        <h3>Search</h3>
+        <p>Find your dream home in the perfect area near your university.</p>
+        </div>
+        <div className="detail-icon-card"><img src={todoList} />
+        <h3>Search</h3>
+        <p>Find your dream home in the perfect area near your university.</p></div>
+        <div className="detail-icon-card">
+            <img src={billList} />
+        <h3>Search</h3>
+        <p>Find your dream home in the perfect area near your university.</p>
+        </div>
+      </div>
     </div>
   )
 }
