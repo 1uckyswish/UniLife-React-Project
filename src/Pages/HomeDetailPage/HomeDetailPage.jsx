7b2440439722c3fa909f693629f162a6d27908bc
Noel Guillen <noelblas274@gmail.com>
2023-06-24 02:38:48 -0400

    added starter code for homepage

--- src/Pages/HomeDetailPage/HomeDetailPage.jsx
+++ src/Pages/HomeDetailPage/HomeDetailPage.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
 import "./HomeDetailPage.css"
+import axios from 'axios'
+import { useParams } from 'react-router-dom'
 
 function HomeDetailPage() {
-    
+  const { propertyid } = useParams();
+  const [homeData, setHomeData] = useState([]);
+
+  useEffect(
+    ()=>{
+      axios.get(`https://unilife-server.herokuapp.com/properties/${propertyid}`)
+      .then((result)=>{
+        console.log(result.data);
+        setHomeData(result.data);
+      })
+      .catch((error)=> console.log(error));
+    },[]
+  )
+
+
   return (
-    <div className='home-details-container'>
-      <h1>Homepage detail</h1>
+    <>
+      <p>return back</p>
+      <div className='home-details-container'>
+      <div className="home-details-left-column">
+        <img src={homeData.images[0]}/>
+      </div>
+      <div className="home-details-right-column">
+      </div>
     </div>
+    </>
   )
 }
 
