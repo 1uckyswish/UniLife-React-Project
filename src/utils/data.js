335a82a8c605b5e7af0245d8290325e57d54483e
Noel Guillen <noelblas274@gmail.com>
2023-07-02 14:53:57 -0400

    fixed data file typo

--- src/utils/data.js
+++ src/utils/data.js
@@ -2,13 +2,13 @@ const data = [
   {
     id: 1,
     title: "Find student homes with bills included",
-    description: "A simple and faster way to search for student accommodation",
+    description: "A simple and faster way to search for student accommodation's.",
   },
   {
     id: 2,
     title: "Student Accomodation",
     description:
-      "UniLife have student accommodation available across the UK. Whatever you’re after, we can help you find the right student accommodation for you",
+      "UniLife has student accommodation's available across the UK. Whatever you’re after, we can help you find the right student accommodation for you.",
   },
   {
     id: 3,
